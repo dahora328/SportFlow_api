8bc8927347ad172cc64afd16ded0607a03ceef44
dahora328 <rodrigoliveira328@hotmail.com>
2025-09-11 17:11:22 -0300

    Add Policy Athletes

---
+++ app/Policies/AthletesPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Athletes;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AthletesPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Athletes $athletes): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Athletes $athletes): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Athletes $athletes): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Athletes $athletes): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Athletes $athletes): bool
+    {
+        return false;
+    }
+}
