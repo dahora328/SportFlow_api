2b8cd16c8b55926add4965386a9706cb67500c05
dahora328 <rodrigoliveira328@hotmail.com>
2025-12-11 08:36:29 -0300

    Feat: Only user viewer your register

--- app/Models/Athletes.php
+++ app/Models/Athletes.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class Athletes extends Model
 {
@@ -32,6 +34,23 @@ class Athletes extends Model
         'owner_id',
     ];
 
+
+    protected static function booted()
+    {
+        // Ao CRIAR um registro (creating), injeta o ID do usuário logado
+        static::creating(function ($athlete) {
+            if (Auth::check()) {
+                $athlete->owner_id = Auth::id();
+            }
+        });
+
+        // Opcional: Escopo global para que o usuário só veja SEUS atletas
+        static::addGlobalScope('owner', function (Builder $builder) {
+            if (Auth::check()) {
+                $builder->where('owner_id', Auth::id());
+            }
+        });
+    }
     public function owner()
     {
         return $this->belongsTo(User::class, 'owner_id');
