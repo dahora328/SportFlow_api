6b014c5efe95aa88731107f06bbc762208c1ed17
dahora328 <rodrigoliveira328@hotmail.com>
2026-04-21 17:23:25 -0300

    Feat: add id in return array athletes

--- app/Http/Resources/AthletesResource.php
+++ app/Http/Resources/AthletesResource.php
@@ -15,6 +15,7 @@ class AthletesResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'full_name' => $this->full_name,
             'birth_date' => $this->birth_date,
             'marital_status' => $this->marital_status,
