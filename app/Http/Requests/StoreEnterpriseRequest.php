ea11d0d38afc203411a07e159b67bfdb208f2bd4
dahora328 <rodrigoliveira328@hotmail.com>
2026-08-11 18:31:11 -0300

    .

--- app/Http/Requests/StoreEnterpriseRequest.php
+++ app/Http/Requests/StoreEnterpriseRequest.php
@@ -27,10 +27,10 @@ class StoreEnterpriseRequest extends FormRequest
             'name' => 'required|string|max:255',
             'social_reason' =>  'required|string|max:255',
             'fantasy_name' => 'required|string|max:255',
-            'owner_name' => 'required|string|max:255',
+            'owner_name' => 'nullable|string|max:255',
             'document' => ['required', 'string', 'max:255', new CpfCnpjRule()],
             'foundation_date' => 'required|date',
-            'IE' => 'required|string|max:255',
+            'IE' => 'nullable|string|max:255',
             'address' => 'required|string|max:255',
             'number' => 'required|string|max:255',
             'complement' => 'nullable|string|max:255',
@@ -41,7 +41,7 @@ class StoreEnterpriseRequest extends FormRequest
             'phone' => 'required|string|max:255',
             'email' => 'required|email|unique:enterprises,email',
             'logo_path' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'active' => 'required|boolean',
+            'active' => 'nullable|boolean',
         ];
     }
 
